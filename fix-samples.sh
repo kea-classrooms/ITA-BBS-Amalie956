2f005ddf44e76b06cbed92ba27557cfe73e5f3a9
Amalie956 <112120321+Amalie956@users.noreply.github.com>
2023-03-15 14:39:37 +0000

    Done done

---
+++ fix-samples.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+cd samples/
+mv animals/countries/ .
+mv countries/movies/ .
+mv animals/Monkies.txt animals/Monkeys.txt 
+cat animals/Monkeys.txt 
+echo animals/Monkeys.txt 
+echo Monkeys > animals/Monkeys.txt 
+mv movies/Germany.txt countries/
+mv countries/.apes.txt animals/
+cd -
